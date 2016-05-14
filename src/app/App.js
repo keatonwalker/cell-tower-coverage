aa6050368b91fd69219c707a18683ba4281900f0
kwalker9 <klwalker9@gmail.com>
2016-05-13 18:37:15 -0600

    Coverage areas can now be created.

--- src/app/App.js
+++ src/app/App.js
@@ -94,49 +94,49 @@ define([
                     map: this.map,
                     centerContainer: this.centerContainer
                 }, this.sidebarToggle),
-                new FindAddress({
-                    map: this.map,
-                    apiKey: config.apiKey,
-                    zoomLevel: 17,
-                    wkid: 3857
-                }, this.geocodeNode),
-                new MagicZoom({
-                    map: this.map,
-                    apiKey: config.apiKey,
-                    wkid: 3857,
-                    searchField: 'NAME',
-                    placeHolder: 'place name...',
-                    maxResultsToDisplay: 10,
-                    'class': 'first'
-                }, this.gnisNode),
-                new MagicZoom({
-                    map: this.map,
-                    apiKey: config.apiKey,
-                    searchLayer: 'SGID10.Boundaries.Municipalities',
-                    searchField: 'NAME',
-                    placeHolder: 'city name...',
-                    wkid: 3857,
-                    maxResultsToDisplay: 10
-                }, this.cityNode),
-                this.printer = new Print({
-                    map: this.map,
-                    url: config.exportWebMapUrl,
-                    templates: [{
-                        label: 'Portrait (PDF)',
-                        format: 'PDF',
-                        layout: 'Letter ANSI A Portrait',
-                        options: {
-                            legendLayers: []
-                        }
-                    }, {
-                        label: 'Landscape (PDF)',
-                        format: 'PDF',
-                        layout: 'Letter ANSI A Landscape',
-                        options: {
-                            legendLayers: []
-                        }
-                    }]
-                }, this.printDiv),
+                // new FindAddress({
+                //     map: this.map,
+                //     apiKey: config.apiKey,
+                //     zoomLevel: 17,
+                //     wkid: 3857
+                // }, this.geocodeNode),
+                // new MagicZoom({
+                //     map: this.map,
+                //     apiKey: config.apiKey,
+                //     wkid: 3857,
+                //     searchField: 'NAME',
+                //     placeHolder: 'place name...',
+                //     maxResultsToDisplay: 10,
+                //     'class': 'first'
+                // }, this.gnisNode),
+                // new MagicZoom({
+                //     map: this.map,
+                //     apiKey: config.apiKey,
+                //     searchLayer: 'SGID10.Boundaries.Municipalities',
+                //     searchField: 'NAME',
+                //     placeHolder: 'city name...',
+                //     wkid: 3857,
+                //     maxResultsToDisplay: 10
+                // }, this.cityNode),
+                // this.printer = new Print({
+                //     map: this.map,
+                //     url: config.exportWebMapUrl,
+                //     templates: [{
+                //         label: 'Portrait (PDF)',
+                //         format: 'PDF',
+                //         layout: 'Letter ANSI A Portrait',
+                //         options: {
+                //             legendLayers: []
+                //         }
+                //     }, {
+                //         label: 'Landscape (PDF)',
+                //         format: 'PDF',
+                //         layout: 'Letter ANSI A Landscape',
+                //         options: {
+                //             legendLayers: []
+                //         }
+                //     }]
+                // }, this.printDiv),
                 new CoordinateSectorCreator({
                     map: this.map
                 }, this.coordinateNode)
@@ -165,9 +165,9 @@ define([
                 widget.startup();
             });
 
-            this.printer.on('print-complete', function () {
-                domStyle.set(that.popupBlockerBlurb, 'display', 'block');
-            });
+            // this.printer.on('print-complete', function () {
+            //     domStyle.set(that.popupBlockerBlurb, 'display', 'block');
+            // });
 
             this.inherited(arguments);
         },
